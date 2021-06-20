5e0895fb0d129ec86d7c5ab677e548a5127b612d
Nagah <nagah.shinawy@gmail.com>
2021-06-20 08:57:25 +0300

    init commit & config

---
+++ config.py
@@ -0,0 +1,10 @@
+"""
+created by Nagaj at 20/06/2021
+"""
+
+IMG = "./data/states.gif"
+STATES = "./data/states.csv"
+
+
+def turtle_setup():
+    pass
---
+++ main.py
@@ -0,0 +1,11 @@
+"""
+created by Nagaj at 20/06/2021
+"""
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
