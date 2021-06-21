24bb4e3484c6a7fa86162c2e905c346e6e93c721
Nagah <nagah.shinawy@gmail.com>
2021-06-21 08:24:47 +0300

    add report

--- main.py
+++ main.py
@@ -5,22 +5,33 @@ import turtle
 from ui import setup, get_state_answer
 from state import State
 
-MAX_STATES = 50
+MAX_STATES = 5
+
+
+def generate_report(lines):
+    with open("./data/report.txt", 'w') as f:
+        for line in lines:
+            f.write(line)
 
 
 def main():
+    lines = []
     tries = 1
     score = 0
     screen = setup()
     state = State()
     while tries <= MAX_STATES:
+        success_fail = "Fail"
         answer = get_state_answer(screen, score)
         coors = state.check_coordinates(answer)
         if coors:
             state.add_to_screen()
             score += 1
-
+            success_fail = "Success"
+        line = f"{answer}[{success_fail}]\n"
+        lines.append(line)
         tries += 1
+    generate_report(lines)
     turtle.mainloop()  # keeping screen open
 
 
